6072329dc1479ec0be164b0aee582721f788974b
Bui Huu Tai <huutaiii2410@gmail.com>
2026-07-10 18:50:34 +0700

    refactor ingestion.py and fetch zone.csv

--- src/ingestion/ingestion.py
+++ src/ingestion/ingestion.py
@@ -4,60 +4,79 @@ from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
 
-def check_object_existence(s3_client, bucket, key) -> bool:
+BUCKET_NAME = "bronze"
+
+def check_object_existence(s3_client, bucket, s3_key) -> bool:
     """
-    This function check if the file is already exist in MinIO to avoid ingesting 
-    duplicate file.
+    Check if the file already exists in MinIO to avoid duplicate ingestion.
     """
     try:
-        s3_client.head_object(
-            Bucket = bucket,
-            Key = key
-        )
+        s3_client.head_object(Bucket=bucket, Key=s3_key)
         return True
-    except:
+    except Exception:
         return False
 
-def generate_file_info(year, month)->dict:
+def generate_file_info(year, month) -> dict:
     """
     Generate the info of parquet file, used for ingestion.
     """
     return {
-            "file_name": f"yellow_tripdata_{year}-{month:02d}.parquet",
-            "url": f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet",
-            "s3_key": f"{year}/{month:02d}/yellow_tripdata_{year}-{month:02d}.parquet"
-            }
+        "url": f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet",
+        "s3_key": f"{year}/{month:02d}/yellow_tripdata_{year}-{month:02d}.parquet"
+    }
+
+def load_data(s3_client, bucket, url, s3_key):
+    """
+    Download data and stream load into MinIO.
+    """
+    with requests.get(url, stream=True, timeout=30) as r:
+        if r.status_code == 200:
+            s3_client.upload_fileobj(
+                Bucket=bucket,  
+                Key=s3_key,
+                Fileobj=r.raw
+            )
+            logger.info(f"Successfully ingested {s3_key}")
+        else:
+            logger.error(f"Failed Ingesting {s3_key}")
+
+
+def process_and_load(s3_client, url, s3_key):
+    """
+    Check if the data already exists before loading data
+    """
+    if check_object_existence(s3_client, BUCKET_NAME, s3_key):
+        logger.warning(f"{s3_key} already exists, skipping")
+        return 
+    
+    logger.info(f"Ingesting {s3_key}")
+    load_data(s3_client, BUCKET_NAME, url, s3_key)
+
+  
 
 def ingest_data(years):
     """
     Ingest all data from 2009 to 2026 on NYC Taxi, and store into bronze of MinIO.
     """
-
     s3_client = get_minio_client()    
 
-        
+    zone_url = "https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv"
+    zone_key = "misc/taxi_zone_lookup.csv"
+
+    try:
+        process_and_load(s3_client, url=zone_url, s3_key=zone_key)
+    except Exception as e:
+        logger.error(f"Error ingesting zone lookup: {str(e)}")
+
     for year in years:
         for month in range(1, 13):
-            
             file_info = generate_file_info(year, month)
-
             try:
-                if check_object_existence(s3_client, "bronze", file_info["s3_key"]):
-                    logger.warning(f"{file_info["s3_key"]} is already exist, skip")
-                    continue
-
-                with requests.get(file_info["url"], stream= True) as r:
-                    if r.status_code == 200:                        
-                        s3_client.upload_fileobj(
-                            Bucket="bronze",
-                            Key=file_info["s3_key"],
-                            Fileobj=r.raw
-                        )
-                        logger.info(f"Ingested {file_info["file_name"]} to {file_info["s3_key"]}")
-                    else:
-                        logger.error(f"Failed Ingesting {file_info["file_name"]} to {file_info["s3_key"]}")
-                        continue
-                        
+                process_and_load(
+                        s3_client=s3_client, 
+                        url=file_info["url"], 
+                        s3_key=file_info["s3_key"]
+                    )
             except Exception as e:
-                raise 
-
+                logger.error(f"Error ingesting {year}-{month:02d}: {str(e)}")
+                continue
